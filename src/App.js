c35f86fbd575d4eaa126e40ca9bf6d743144b3be
gautam417 <gautammehta@Gautams-MacBook.local>
2020-05-18 12:24:59 -0700

    Adding about section content

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import Home from './contents/Home';
 function App() {
   return (
     <Router>
     <div className="App">
     <Navbar></Navbar>
+    <Route exact path="/">
+      <Home></Home>
+    </Route>
     </div>
     </Router>
   );
--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,4 +1,4 @@
-import React, ( Component ) from 'react';
+import React, { Component } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import profilepic from '../img/B&W.jpg';
 import Social from '../components/Social';
@@ -9,7 +9,7 @@ class Home extends Component {
       <div className='condiv home'>
       <img src={profilepic} className='profilepic'></img>
       <ReactTypingEffect className='typingeffect' text={['I am Gautam Mehta', 'I am a Software Engineer', 'Hello World']} speed={100} eraseDelay={700}/>
-      <Social></Social> 
+      <Social></Social>
       </div>
     )
   }
