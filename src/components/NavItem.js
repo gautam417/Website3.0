23b6fc761723c5fa844db075dfb45ff00607b161
gautam417 <gautammehta@Gautams-MacBook.local>
2020-05-18 01:58:51 -0700

    Add css and callback for active state

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -5,11 +5,12 @@ import { Link } from "react-router-dom";
 class Navitem extends Component {
   render() {
     return (
-        <li>
+        <li id={this.props.item}>
         {/* Gets the value of item and tolink props
-            Stores into Link Component */}
-        <Link to={this.prop.tolink} >
-        {this.props.item}
+            Stores into Link Component
+            to attribute is similar to href in an anchor tag*/}
+        <Link to={this.props.tolink} onClick=
+        {this.props.activec.bind(this,this.props.item)}>{this.props.item}
         </Link>
         </li>
     )
